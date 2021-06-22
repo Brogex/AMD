b0116e357b8e73c93231e46c53286557cb306ce5
Brogex <53215244+Brogex@users.noreply.github.com>
2021-06-22 15:20:03 +0200

    Create amd.js

---
+++ amd.js
@@ -0,0 +1,9 @@
+// Billing address
+document.getElementById('edit-email').value = 'ducciobrogelli@gmail.com';
+document.getElementById('edit-phone-number').value = '3294525629';
+document.getElementById('edit-first-name').value = 'Francesca';
+document.getElementById('edit-last-name').value = 'Ferroni';
+document.getElementById('edit-shop-country').value = 'IT';
+document.getElementById('edit-address-line').value = 'Via del mezzetta 5';
+document.getElementById('edit-city').value = 'Firenze';
+document.getElementById('edit-postal-code').value = '50135';
